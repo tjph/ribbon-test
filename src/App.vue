b343af6af2840809e64ff8f834cf7f09c8418b18
TH <th@THs-MacBook-Air.local>
2023-03-30 14:19:05 -0700

    move donor list to own component

--- src/App.vue
+++ src/App.vue
@@ -25,43 +25,7 @@
         </v-sheet>
       </section>
 
-      <v-sheet>
-        <section id="filter">
-          <v-container>
-            <v-row justify="space-between">
-              <v-col cols="auto">
-                <h2 class="text-h4">Ribbon Donor List</h2>
-
-                <p class="text-primary mt-3">In Beta now!</p>
-
-                <p class="mt-3">See all those that have given in one place!</p>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
-                <table v-if="donors">
-                  <thead>
-                    <tr>
-                      <th class="text-left">Name</th>
-                      <th class="text-left">Email</th>
-                      <th class="text-left">Total Donations</th>
-                      <th class="text-left">First Donation</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr v-for="item in donors.data" :key="item.id">
-                      <td>{{ item.full_name }}</td>
-                      <td>{{ item.email }}</td>
-                      <td>{{ item.total_donations }}</td>
-                      <td>{{ item.first_donation }}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </v-col>
-            </v-row>
-          </v-container>
-        </section>
-      </v-sheet>
+      <DonorsList />
 
       <v-sheet class="py-16" color="#1818181a">
         <section id="grid">
@@ -118,13 +82,16 @@
 </template>
 
 <script>
-import axios from "axios";
+import DonorsList from './components/DonorList';
+
 export default {
   name: "App",
+  components: {
+    DonorsList
+  },
 
   data() {
     return {
-      donors: null,
       valid: false,
       email: "",
       donor_id: "",
@@ -144,11 +111,6 @@ export default {
         },
       ],
     };
-  },
-  mounted() {
-    axios
-      .get("https://interview.ribbon.giving/api/donors")
-      .then((response) => (this.donors = response.data));
   },
   methods: {
     async submit() {
---
+++ src/components/DonorList.vue
@@ -0,0 +1,58 @@
+<template>
+      <v-sheet>
+        <section id="filter">
+          <v-container>
+            <v-row justify="space-between">
+              <v-col cols="auto">
+                <h2 class="text-h4">Ribbon Donor List</h2>
+
+                <p class="text-primary mt-3">In Beta now!</p>
+
+                <p class="mt-3">See all those that have given in one place!</p>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <table v-if="donors">
+                  <thead>
+                    <tr>
+                      <th class="text-left">Name</th>
+                      <th class="text-left">Email</th>
+                      <th class="text-left">Total Donations</th>
+                      <th class="text-left">First Donation</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="item in donors.data" :key="item.id">
+                      <td>{{ item.full_name }}</td>
+                      <td>{{ item.email }}</td>
+                      <td>{{ item.total_donations }}</td>
+                      <td>{{ item.first_donation }}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </v-col>
+            </v-row>
+          </v-container>
+        </section>
+      </v-sheet>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+	name: 'DonorList',
+
+	data() {
+		return {
+		donors: null
+		};
+	},
+	mounted() {
+		axios
+		.get("https://interview.ribbon.giving/api/donors")
+		.then((response) => (this.donors = response.data));
+	}
+}
+</script>
